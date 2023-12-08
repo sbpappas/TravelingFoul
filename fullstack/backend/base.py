542ee59718dfe3a473d14a199077101d123d41bb
Jeremy Brigham <42706073+Jbrigham-Trinity@users.noreply.github.com>
2023-12-08 14:17:18 -0600

    finalization of front end files

--- fullstack/backend/base.py
+++ fullstack/backend/base.py
@@ -5,8 +5,7 @@ api = Flask(__name__)
 @api.route('/profile')
 def my_profile():
     response_body = {
-        "name": "Jimbert Bingus",
-        "about" :"Mom help I don't know how react works"
+        "name": "Test",
     }
     return response_body
 
